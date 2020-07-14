d5e8971f1a8a3f92f5ca34d34ad46b4afa8e05bf
Vicky Arya Pratama <68258264+VickyArpa@users.noreply.github.com>
2020-07-14 10:20:19 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const humberger = document.querySelector('.humberger-menu input');
-const nav = document.querySelector('nav ul');
-
-humberger.addEventListener('click', function () {
-    nav.classList.toggle('slide');
-})
